bd2c9d8ee9a3b186537a85defbd793260bbeeafb
huangyingfei <565267598@qq.com>
2019-07-22 10:25:42 +0800

    update

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-#!/usr/bin/python3
-""" Demonstrating Flask, using APScheduler. """
-
+import os
 from flask import Flask, render_template
+from flask import request, jsonify, send_from_directory, abort
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    """ Function for test purposes. """
     return render_template("index.html")
 
 
+@app.route('/download_file/<filename>')
+def download_filename(filename):
+    if request.method == "GET":
+        if os.path.isfile(os.path.join('static/files', filename)):
+            return send_from_directory('static/files', filename, as_attachment=True)
+        abort(404)
+
+
+@app.route('/download')
+def download():
+    if request.method == "GET":
+        if os.path.isfile(os.path.join('static/files', "pox_wallet.apk")):
+            return send_from_directory('static/files', "pox_wallet.apk", as_attachment=True)
+        abort(404)
+
+
 if __name__ == "__main__":
-    app.run(port=9000)
+    app.run(port=9000, host="0.0.0.0", debug=False)
